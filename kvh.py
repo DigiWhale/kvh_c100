601ca28f60d6c453edb61ab6a8dbbe145df17766
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-21 15:49:53 -0500

    1

--- kvh.py
+++ kvh.py
@@ -1,9 +1,23 @@
-import serial 
+import serial
+
+def readline(ser):
+    eol = b'\r'
+    leneol = len(eol)
+    line = bytearray()
+    while True:
+        c = ser.read(1)
+        if c:
+            line += c
+            if line[-leneol:] == eol:
+                break
+        else:
+            break
+    return bytes(line) 
 
 try:
   ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
   while ser:
-    print(ser.read(19))
+    print(readline(ser))
 except Exception as e:
   print(e)
   ser.close()
