77d63f5de3d42b09e2bddf51acc8d20943d8a1da
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-21 15:47:51 -0500

    1

--- kvh.py
+++ kvh.py
@@ -3,7 +3,7 @@ import serial
 try:
   ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
   while ser:
-    print(ser.read(19).decode('utf-8'))
+    print(ser.read(19))
 except Exception as e:
   print(e)
   ser.close()
