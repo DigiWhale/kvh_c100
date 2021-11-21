50f8c1912aa0d3e0bc424157ef8562b361253909
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-21 16:48:17 -0500

    1

--- kvh.py
+++ kvh.py
@@ -19,7 +19,8 @@ try:
   ser = serial.Serial('/dev/ttyS0', 4800, bytesize=8, parity='N', stopbits=1, timeout=1)
   while ser:
     # ser.write(b'd0\r')
-    ser.write(b'64\30\r')
+    msg = bytearray([64, 30])
+    ser.write(msg)
     print(ser.readline().decode('ascii', errors='replace'))
     # print(ser.read(19))
     # print(int(ser.read(1).hex(), 16))
