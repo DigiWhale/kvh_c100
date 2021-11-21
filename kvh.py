575e49eb6342f57f7cc8e01c3e7f16ba61c28432
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-21 16:48:54 -0500

    1

--- kvh.py
+++ kvh.py
@@ -20,8 +20,9 @@ try:
   while ser:
     # ser.write(b'd0\r')
     msg = bytearray([64, 30])
+    print(msg)
     ser.write(msg)
-    print(ser.readline().decode('ascii', errors='replace'))
+    print(ser.read(19).decode('ascii', errors='replace'))
     # print(ser.read(19))
     # print(int(ser.read(1).hex(), 16))
     # print(bytearray.fromhex(ser.read(1).hex()).decode())
