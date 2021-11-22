30d484cb17bba304a43d497a259c236da075ccb3
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-21 19:56:28 -0500

    1

--- kvh.py
+++ kvh.py
@@ -4,19 +4,21 @@ try:
   ser = serial.Serial('/dev/ttyS0', 4800, bytesize=8, parity='N', stopbits=1, timeout=1)
   while ser:
     # msg = bytearray([100, 48, 13])
-    msg = bytearray([104, 13])
-    ser.write(msg)
-    hex_data = ser.read(19).hex()
-    print('hex_data', hex_data)
-    ascii_string = ''
-    x = 0
-    y = 2
-    l = len(hex_data)
-    while y <= l:
-        ascii_string += chr(int(hex_data[x:y], 16))
-        x += 2
-        y += 2
-    print ("ascii_string", ascii_string)
+    # msg = bytearray([104, 13])
+    # ser.write(msg)
+    # hex_data = ser.read(19).hex()
+    # print('hex_data', hex_data)
+    # ascii_string = ''
+    # x = 0
+    # y = 2
+    # l = len(hex_data)
+    # while y <= l:
+    #     ascii_string += chr(int(hex_data[x:y], 16))
+    #     x += 2
+    #     y += 2
+    # print ("ascii_string", ascii_string)
+    i = int(ser.read(1), 16)
+    print(str(i))
 except Exception as e:
   print(e)
   ser.close()
