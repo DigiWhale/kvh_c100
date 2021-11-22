259a9ab48e235f4409b42d9c76bcad242434b26e
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-21 20:28:15 -0500

    1

--- kvh.py
+++ kvh.py
@@ -4,7 +4,7 @@ try:
   ser = serial.Serial('/dev/ttyS0', 4800, bytesize=8, parity='N', stopbits=1, timeout=1)
   heading = 0
   while ser:
-    init_heading = ser.read(19).decode('utf-8').split(',')[1]
+    init_heading = int(ser.read(19).decode('utf-8').split(',')[1])
     if init_heading < 361:
       heading = init_heading
     print(heading)
