d8beae934d0eec3546f5aedc45827e927bdf65ad
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-24 13:15:34 -0500

    1

--- kvh.py
+++ kvh.py
@@ -8,7 +8,9 @@ class Kvh_Compass:
     # self.ser.write(b'r,6\r\n')
     
   def set_msg_rate(self, rate):
-    self.ser.write(f'=r,{rate}\r\n'.encode())
+    msg = f'=r,{rate}\r\n'.encode()
+    print(msg)
+    self.ser.write(msg)
     print(self.ser.readline())
     print(self.ser.readline())
     print('Set rate to {}'.format(rate))
