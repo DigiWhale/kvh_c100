e54cffd49722ec1577feb997f2840fcbd2e4cb60
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-24 17:40:57 -0500

    1

--- kvh_compass.py
+++ kvh_compass.py
@@ -39,6 +39,9 @@ class Kvh_Compass:
     time.sleep(.2)
     self.ser.write(b'?cs1\r') # read variation
     print("calibration score = ", self.ser.readline())
+    time.sleep(.2)
+    self.ser.write(b'?w\r') # read variation
+    print("calibration date = ", self.ser.readline())
     
   def get_heading(self):
     self.ser.write(b'd0\r\n')
