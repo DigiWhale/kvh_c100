3ad15ff2728d213b92859cc04bea4f06ebf26ec4
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-24 13:31:10 -0500

    1

--- kvh.py
+++ kvh.py
@@ -4,8 +4,8 @@ import time
 class Kvh_Compass:
   def __init__(self, port):
     self.ser = serial.Serial(port, 4800, bytesize=8, parity='N', stopbits=1, timeout=1)
-    # self.ser.write(b'h\r\n')
-    self.ser.write(b's\r\n')
+    self.ser.write(b'h\r\n')
+    # self.ser.write(b's\r\n')
     # self.ser.write(b'r,6\r\n')
     
   def set_msg_rate(self, rate):
