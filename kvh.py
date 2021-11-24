4ce2853056379d8ba71d11bde54d8b9dafcd0936
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-24 12:48:11 -0500

    1

--- kvh.py
+++ kvh.py
@@ -1,16 +1,33 @@
 import serial
+import time
 
-try:
-  ser = serial.Serial('/dev/ttyS0', 4800, bytesize=8, parity='N', stopbits=1, timeout=1)
-  heading = 0
-  while ser:
-    # init_heading = float(ser.read(19).decode('utf-8').split(',')[1])
-    # if init_heading < 361:
-    #   heading = init_heading
-    # print(heading)
-    ser.write(b'?w\r\n')
-    print(ser.readline())
+class Kvh_Compass:
+  def __init__(self, port):
+    self.ser = serial.Serial(port, 4800, bytesize=8, parity='N', stopbits=1, timeout=1)
+    self.ser.write(b'h\r\n')
+    
+  def get_heading(self):
+    self.ser.write(b'd0\r\n')
+    nmea_sentence = self.ser.readline()
+    return nmea_sentence
 
-except Exception as e:
-  print(e)
-  ser.close()
+# try:
+#   ser = serial.Serial('/dev/ttyS0', 4800, bytesize=8, parity='N', stopbits=1, timeout=1)
+#   heading = 0
+#   while ser:
+#     # init_heading = float(ser.read(19).decode('utf-8').split(',')[1])
+#     # if init_heading < 361:
+#     #   heading = init_heading
+#     # print(heading)
+#     ser.write(b'?w\r\n')
+#     print(ser.readline())
+
+# except Exception as e:
+#   print(e)
+#   ser.close()
+  
+if __name__ == '__main__':
+  kvh_compass = Kvh_Compass('/dev/ttyS0')
+  while True:
+    print(kvh_compass.get_heading())
+    time.sleep(1)
