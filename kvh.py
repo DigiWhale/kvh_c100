adb20c8055ce56cef4ffff87108654a208acfa2c
digiwhale <digiwhale.humphrey@gmail.com>
2021-11-24 13:44:07 -0500

    1

--- kvh.py
+++ kvh.py
@@ -6,7 +6,7 @@ class Kvh_Compass:
     self.ser = serial.Serial(port, 4800, bytesize=8, parity='N', stopbits=1, timeout=1)
     # self.ser.write(b'h\r\n')
     self.ser.write(b's\r\n')
-    # self.ser.write(b'r,6\r\n')
+    self.ser.write(b'r,600\r\n')
     
   def set_msg_rate(self, rate):
     msg = b'=r,600\r'
@@ -51,7 +51,6 @@ class Kvh_Compass:
   
 if __name__ == '__main__':
   kvh_compass = Kvh_Compass('/dev/ttyS0')
-  kvh_compass.set_msg_rate('600')
   while True:
     # print(kvh_compass.get_heading())
     # time.sleep(1)
